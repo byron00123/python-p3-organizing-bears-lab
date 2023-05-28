656b8eb6ec78cae4cc7f69d1e61a2c7a0b829d91
Byron Omondi <omondibyron25@gmail.com>
2023-05-28 10:41:03 +0300

    complete assingment

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, 'Rowdy', 3, 'M', 'Black', 'Playful', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, 'Tabitha', 4, 'F', 'Brown', 'Calm', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, 'Sergeant Brown', 7, 'M', 'Brown', 'Bold', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, 'Melissa', 2, 'F', 'White', 'Energetic', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, 'Grinch', 6, 'M', 'Green', 'Mischievous', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, 'Wendy', 4, 'F', 'Black', 'Friendly', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, NULL, NULL, NULL, NULL, NULL, false);
+
